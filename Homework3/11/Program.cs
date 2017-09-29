7c881368bdd4c07b3cd573ea768c5274a2ae5bf8
morskibg <dimityrp@yahoo.com>
2017-09-29 21:50:23 +0200

    commit

---
+++ Homework3/11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int distInMetre = int.Parse(Console.ReadLine());
+            int hours = int.Parse(Console.ReadLine());
+            int minute = int.Parse(Console.ReadLine());
+            int seconds = int.Parse(Console.ReadLine());
+            float totalSeconds = (float)hours * 60 * 60 + (float)minute * 60 + seconds;
+            float speedMetrePerSecond = distInMetre / totalSeconds;
+            Console.WriteLine(speedMetrePerSecond);
+            Console.WriteLine(speedMetrePerSecond * 3600 / 1000);
+            Console.WriteLine(speedMetrePerSecond * 3600 / 1609);
+
+
+
+        }
+    }
+}
---
+++ Homework3/13/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char input = Console.ReadLine()[0];
+            if (char.IsDigit(input))
+            {
+                Console.WriteLine("digit");
+            }
+            else if (input == 'a' || input == 'o' || input == 'e' ||
+                     input == 'i' || input == 'u')
+            {
+                Console.WriteLine("vowel");
+            }
+            else
+            {
+                Console.WriteLine("other");
+            }
+        }
+    }
+}
