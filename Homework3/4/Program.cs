845048042995d7e16b059563110bb1a08b3fd9f0
morskibg <dimityrp@yahoo.com>
2017-09-29 20:40:10 +0200

    commit

---
+++ Homework3/4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string input = Console.ReadLine();
+            Console.WriteLine(Convert.ToInt32(Console.ReadLine(), 16));
+        }
+    }
+}
---
+++ Homework3/6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstWord = "Hello";
+            string lastWord = "World";
+            object resulString = firstWord + " " + lastWord;
+            Console.WriteLine(resulString);
+
+        }
+    }
+}
