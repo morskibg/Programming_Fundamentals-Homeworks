91d5e8533c87bac7e54922c213c65077647ebeef
morskibg <dimityrp@yahoo.com>
2017-09-29 21:59:55 +0200

    commit

---
+++ Homework3/14/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string hex = Convert.ToString(n, 16);
+            hex = hex.ToUpper();
+            Console.WriteLine(hex);
+            Console.WriteLine(Convert.ToString(n, 2));
+        }
+    }
+}
