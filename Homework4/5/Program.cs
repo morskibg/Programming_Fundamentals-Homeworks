a30a2283b4fed32530ba883d6d7f37840df40311
morskibg <dimityrp@yahoo.com>
2017-10-07 11:00:16 +0200

    +

--- Homework4/5/Program.cs
+++ Homework4/5/Program.cs
@@ -8,6 +8,18 @@ namespace _5
 {
     class Program
     {
+        static void PrintFirstArr(char[] arr1, char[] arr2)
+        {
+            Console.WriteLine($"{string.Join("", arr1)}");
+            Console.WriteLine($"{string.Join("", arr2)}");
+           
+        }
+        static void PrintSecondArr(char[] arr1, char[] arr2)
+        {
+            Console.WriteLine($"{string.Join("", arr2)}");
+            Console.WriteLine($"{string.Join("", arr1)}");
+            
+        }
         static void Main(string[] args)
         {
             
@@ -15,23 +27,35 @@ namespace _5
             char[] arr2 = Console.ReadLine().ToLower().Split(' ').Select(x => x[0]).ToArray();
             int idx = 0;
             int minSize = Math.Min(arr1.Length, arr2.Length);
-            while (true)
+            while (idx < minSize)
             {
                 if (arr1[idx] > arr2[idx])
                 {
-                    Console.WriteLine($"{string.Join("", arr2)}");
-                    Console.WriteLine($"{string.Join("", arr1)}");
+                    PrintSecondArr(arr1, arr2);
                     return;
                 }
                 else if (arr1[idx] < arr2[idx])
                 {
-                    Console.WriteLine($"{string.Join("", arr1)}");
-                    Console.WriteLine($"{string.Join("", arr2)}");
+                    PrintFirstArr(arr1, arr2);
                     return;
                 }
                 ++idx;
             }
-            
+            if (arr1.Length == arr2.Length)
+            {
+                PrintFirstArr(arr1, arr2);
+                return;
+            }
+            else if (arr1.Length > arr2.Length)
+            {
+                PrintSecondArr(arr1, arr2);
+                return;
+            }
+            else
+            {
+                PrintFirstArr(arr1, arr2);
+                return;
+            }
         }
     }
 }
---
+++ Homework4/6/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] initArr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int bestStartIdx = 0;
+            int bestLen = 0;
+            int bestNum = initArr[0];
+            int tempLen = 1;
+            int tempIdx = 0;
+            for (int i = 1; i < initArr.Length; i++)
+            {
+                if (bestNum == initArr[i])
+                {
+                    ++tempLen;
+                }
+                else
+                {
+                    if (tempLen > bestLen)
+                    {
+                        bestLen = tempLen;
+                        bestStartIdx = tempIdx;
+                    }
+                    tempLen = 1;
+                    tempIdx = i;
+                    bestNum = initArr[i];
+                }
+            }
+            if (tempLen > bestLen)
+            {
+                bestLen = tempLen;
+                bestStartIdx = tempIdx;
+            }
+            for (int i = bestStartIdx; i < bestStartIdx + bestLen; i++)
+            {
+                Console.Write(initArr[i] + " ");
+            }
+        }
+    }
+}
