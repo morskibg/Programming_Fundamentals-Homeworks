b149cd331ee2f0655c76656cabcf8fb893cc8e77
morskibg <dimityrp@yahoo.com>
2017-10-11 17:53:11 +0300

    +

--- Homework6/5/Program.cs
+++ Homework6/5/Program.cs
@@ -10,6 +10,37 @@ namespace _5
     {
         static void Main(string[] args)
         {
+            List<int> nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            string[] command = Console.ReadLine().Split(' ').ToArray();
+            while(command[0] != "print")
+            {
+                if(command[0] == "add")
+                {
+                    int idx = int.Parse(command[1]);
+                    int element = int.Parse(command[2]);
+                    nums.Insert(idx, element);
+                }
+                else if(command[0] == "addMany")
+                {
+                    int idx = int.Parse(command[1]);
+                    nums.InsertRange(idx, command.Skip(2).Select(int.Parse).ToArray());
+                }
+                else if(command[0] == "contains")
+                {
+                    int element = int.Parse(command[1]);
+                    Console.WriteLine(nums.Find(x => x == element));
+                }
+                else if (command[0] == "remove")
+                {
+                    int idx = int.Parse(command[1]);
+                    nums.RemoveAt(idx);
+                }
+                else if (command[0] == "shift")
+                {
+                    int position = int.Parse(command[1]);
+                    
+                }
+            } 
         }
     }
 }
---
+++ Homework6/7/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            string[] bombRaw = Console.ReadLine().Split(' ').ToArray();
+            int bomb = int.Parse(bombRaw[0]);
+           
+            int bombPower = int.Parse(bombRaw[1]);
+            for(int i = 0; i < nums.Count; ++i)
+            {
+                if(nums[i] == bomb)
+                {
+                    int startIdx = i - bombPower;
+                    int endIdx = i + bombPower;
+                    int fullBlastInterval = 2 * bombPower + 1;
+                    if(startIdx >= 0 && endIdx <= nums.Count - 1)
+                    {
+                        nums.RemoveRange(startIdx, fullBlastInterval);
+                    }
+                    else if(startIdx >= 0 && endIdx > nums.Count - 1)
+                    {
+                        nums = nums.Take(startIdx).ToList();
+                    }
+                    else if(startIdx < 0 && endIdx <= nums.Count - 1)
+                    {
+                        nums = nums.Skip(endIdx + 1).ToList();
+                    }
+                    else
+                    {
+                        nums.Clear();
+                    }
+                    i = 0;
+                }
+            }
+            decimal sum = nums.Sum();
+            Console.WriteLine(sum);
+
+        }
+    }
+}
