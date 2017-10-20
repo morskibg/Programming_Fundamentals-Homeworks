3baac6f2b21c0e9ce3a97919c5e763aed8074787
morskibg <dimityrp@yahoo.com>
2017-10-20 21:23:01 +0200

    +

---
+++ Homework9/6. Fix Emails/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.Fix_Emails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> users = new Dictionary<string, string>();
+            string input = Console.ReadLine();
+            string prevName = "";
+            while (input != "stop")
+            {
+                if (input.Contains("@"))
+                {
+                    char[] delim = ".".ToCharArray();
+                    string[] tokens = input.ToLower().Split(delim, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                    if (tokens.Last() != "us" && tokens.Last() != "uk")
+                    {
+                        users[prevName] = input;
+                    }
+                }
+                else
+                {
+                    prevName = input;
+                }
+                input = Console.ReadLine();
+            }
+            foreach (var user in users)
+            {
+                Console.WriteLine($"{user.Key} -> {user.Value}");
+            }
+        }
+    }
+}
