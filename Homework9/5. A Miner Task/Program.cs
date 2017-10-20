2dfc89264356248c6e75a950ffc8223995b9a775
morskibg <dimityrp@yahoo.com>
2017-10-20 20:58:44 +0200

    +

---
+++ Homework9/5. A Miner Task/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, long> minerData = new Dictionary<string, long>();
+            
+            
+            string input = Console.ReadLine();
+            string lastMetal = "";
+            while (input != "stop")
+            {
+                long parsed;
+                if (!long.TryParse(input, out parsed))
+                {
+                    lastMetal = input;
+                    if (!minerData.ContainsKey(lastMetal))
+                    {
+                        minerData[lastMetal] = 0;
+                    }
+                }
+                else
+                {
+                    minerData[lastMetal] += parsed;
+                }
+                input = Console.ReadLine();
+            }
+            foreach (var metal in minerData)
+            {
+                Console.WriteLine($"{metal.Key} -> {metal.Value}");
+            }
+        }
+    }
+}
