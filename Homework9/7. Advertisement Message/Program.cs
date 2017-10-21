a5d33341e9efb57ef76de0136db3b16ce4f594ba
morskibg <dimityrp@yahoo.com>
2017-10-21 07:50:08 +0200

    +

---
+++ Homework9/7. Advertisement Message/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.Advertisement_Message
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            string[] Phrases = new string[]
+            {
+                "Excellent product.",
+                "Such a great product.",
+                "I always use that product.",
+                "Best product of its category.",
+                "Exceptional product.",
+                "I can’t live without this product."
+            };
+            string[] Events = new string[]
+            {
+                "Now I feel good.",
+                "I have succeeded with this product.",
+                "Makes miracles. I am happy of the results!",
+                "I cannot believe but now I feel awesome.",
+                "Try it yourself, I am very satisfied.",
+                "I feel great!"
+            };
+            string[] Authors = new string[]
+            {
+                "Diana", "Petya", "Stella",
+                "Elena", "Katya", "Iva", "Annie", "Eva"
+            };
+            string[] Cities = new string[]
+            {
+                "Burgas", "Sofia",
+                "Plovdiv", "Varna", "Ruse"
+            };
+            
+            
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"{Phrases[rand.Next(0, Phrases.Length)]} {Events[rand.Next(0, Events.Length)]} " +
+                                  $"{Authors[rand.Next(0, Authors.Length)]} {Cities[rand.Next(0, Cities.Length)]}");
+                //Console.Write($"{Phrases[rand.Next(0, Phrases.Length)]}");
+                //Console.Write($" {Events[rand.Next(0, Events.Length)]}");
+                //Console.Write($" {Authors[rand.Next(0, Authors.Length)]}");
+                //Console.Write($" {Cities[rand.Next(0, Cities.Length)]}");
+                //Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Homework9/8. Average Grades/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.Average_Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Student> database = new List<Student>();
+            for (int i = 0; i < n; i++)
+            {
+                char[] delim = " ".ToCharArray();
+                string[] tokens = Console.ReadLine().Split(delim, StringSplitOptions.RemoveEmptyEntries).ToArray();
+                Student student = new Student();
+                student.Name = tokens[0];
+                for (int j = 1; j < tokens.Length; j++)
+                {
+                    student.Grades.Add(double.Parse(tokens[j]));
+                }
+                database.Add(student);
+            }
+            foreach (var student in database
+                .Where(x => x.AverageGrade >= 5)
+                .OrderBy(x => x.Name)
+                .ThenByDescending(x => x.AverageGrade))
+            {
+                Console.WriteLine($"{student.Name} -> {student.AverageGrade:f2}");
+            }
+        }
+    }
+
+    class Student
+    {
+        public string Name { get; set; }
+        public List<double> Grades { get; set; }
+        public double AverageGrade
+        {
+            get { return Grades.Average(); }
+        }
+
+        public Student()
+        {
+            Grades = new List<double>();
+        }
+    }
+}
