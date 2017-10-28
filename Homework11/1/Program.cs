9f471bd60ded4c48e7424c2fe4ef1e6f4ff4a96f
morskibg <dimityrp@yahoo.com>
2017-10-28 10:26:30 +0200

    +

---
+++ Homework11/1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"(?<=\s|^)\b[a-z0-9][._-]*[a-z0-9]+[._-]*[a-z0-9]+(?![._-])\@[a-z0-9]+[._-]*[a-z0-9]+\.\w+(\.\w+)?";
+            MatchCollection matchedEmails = Regex.Matches(input, pattern);
+            foreach ( Match mail in matchedEmails)
+            {
+                Console.WriteLine(mail);
+            }
+            
+        }
+    }
+}
