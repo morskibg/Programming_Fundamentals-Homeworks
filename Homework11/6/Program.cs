2ffe16851a59892edb1ee57e7da4ecbab5802725
morskibg <dimityrp@yahoo.com>
2017-10-29 15:55:48 +0200

    +

--- Homework11/6/Program.cs
+++ Homework11/6/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace _6
@@ -10,6 +11,35 @@ namespace _6
     {
         static void Main(string[] args)
         {
+            char[] delimiter = " /\\()".ToCharArray();
+            string[] rawUserNAmes =
+                Console.ReadLine().Split(delimiter, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            List<string> validatedUserNames = new List<string>();
+            string pattern = @"^[a-zA-Z]\w{2,24}$";
+            foreach (var userName in rawUserNAmes)
+            {
+                Regex rg = new Regex(pattern);
+                Match currMatch = rg.Match(userName);
+                if (currMatch.Success)
+                {
+                    validatedUserNames.Add(userName);
+                }
+            }
+            string[] longestLenCouple = new string[2];
+            int MaxLengthSum = Int32.MinValue;
+            for (int i = 0; i <= validatedUserNames.Count - 2; i++)
+            {
+                int innerSum = validatedUserNames[i].Length + validatedUserNames[i + 1].Length;
+                if (innerSum > MaxLengthSum)
+                {
+                    longestLenCouple[0] = validatedUserNames[i];
+                    longestLenCouple[1] = validatedUserNames[i + 1];
+                    MaxLengthSum = innerSum;
+                }
+            }
+            Console.WriteLine(longestLenCouple[0]);
+            Console.WriteLine(longestLenCouple[1]);
+           
         }
     }
 }
---
+++ Homework11/6_2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"((?<=^|\s|[()\/\\])[A-Za-z]\w{2,24})\b";
+            MatchCollection matchenUserNames = Regex.Matches(Console.ReadLine(), pattern);
+            int maxLength = Int32.MinValue;
+            StringBuilder[] maxLenCouple = new StringBuilder[2]
+            {
+                new StringBuilder(), 
+                new StringBuilder()
+            };
+            for (int i = 0; i < matchenUserNames.Count - 1; i++)
+            {
+                int innerSumLen = matchenUserNames[i].Length + matchenUserNames[i + 1].Length;
+                if (innerSumLen > maxLength)
+                {
+                    maxLength = innerSumLen;
+                    maxLenCouple[0].Clear();
+                    maxLenCouple[0].Append(matchenUserNames[i]);
+                    maxLenCouple[1].Clear();
+                    maxLenCouple[1].Append(matchenUserNames[i + 1]);
+                }
+            }
+            Console.WriteLine(maxLenCouple[0]);
+            Console.WriteLine(maxLenCouple[1]);
+        }
+    }
+}
