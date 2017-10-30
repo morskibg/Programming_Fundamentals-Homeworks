e931f91606c943a2c07a03d33c1d14ca4e9b5131
morskibg <dimityrp@yahoo.com>
2017-10-30 17:37:20 +0200

    +

---
+++ Homework11/8/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = File.ReadAllText("input.txt");
+            string tagPattern = @"((?<=<p>).*?(?=<\/p>))";
+            var encryptedDataMatchCollection = Regex.Matches(input, tagPattern);
+            var encryptedDataList = new List<string>();
+            foreach (Match currMatch in encryptedDataMatchCollection)
+            {
+                encryptedDataList.Add(currMatch.ToString());
+            }
+            encryptedDataList = encryptedDataList
+                .Select(x => Regex.Replace(x, "[^a-z]", " "))                
+                .ToList();
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < encryptedDataList.Count; ++i)
+            {
+                for(int j = 0; j < encryptedDataList[i].Length; ++j)
+                {
+
+                }
+            }
+            
+            int t = 0;
+        }
+    }
+}
---
+++ Homework11/8_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _8_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                var matches = Regex
+                    .Matches(Console.ReadLine(), @"((?<=<p>).*?(?=<\/p>))");
+
+                var result = new StringBuilder();
+
+                foreach (Match match in matches)
+                {
+                    result.Append(string.Concat(Regex
+                        .Replace(match.Value, @"[^a-z]+", " ")
+                        .ToCharArray()
+                        .Select(chr => (chr >= 'a' && chr <= 'z')
+                            ? (char)('a' + (chr - 'a' + 13) % 26)
+                            : chr)));
+                }
+
+                Console.WriteLine(result.ToString());
+            }
+        }
+    }
+}
